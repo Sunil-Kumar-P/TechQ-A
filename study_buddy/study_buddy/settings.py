3587e24cb9ecdcf212219cb630527b9fdb4908eb
Sunil Kumar P <sunilkpg23@gmail.com>
2022-06-27 18:03:13 +0530

    do it

--- study_buddy/study_buddy/settings.py
+++ study_buddy/study_buddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eb(b55=!%c7#63^15t*@c3#bf31%*rgwjegrxeu2i$fqlu)bpf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','codewithsunil.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','codewithsunil.herokuapp.com']
 
 
 # Application definition
