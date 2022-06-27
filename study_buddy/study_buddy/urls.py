15032d98adb7930c69ccc9f882e80fef117a3695
Sunil Kumar P <sunilkpg23@gmail.com>
2022-06-27 18:00:04 +0530

    dont know the problem

--- study_buddy/study_buddy/urls.py
+++ study_buddy/study_buddy/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from . import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
