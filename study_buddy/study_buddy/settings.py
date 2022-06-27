93b62d1ff9ee0675d2f4f648ae60db3d6947caab
Sunil Kumar P <sunilkpg23@gmail.com>
2022-06-27 17:54:29 +0530

    once more

--- study_buddy/study_buddy/settings.py
+++ study_buddy/study_buddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eb(b55=!%c7#63^15t*@c3#bf31%*rgwjegrxeu2i$fqlu)bpf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000','codewithsunil.herokuapp.com']
 
 
 # Application definition
