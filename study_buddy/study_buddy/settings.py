7bd88d378b6ac23077ad39fcdd675d3654355572
Sunil Kumar P <sunilkpg23@gmail.com>
2022-06-27 17:50:00 +0530

    heroku

--- study_buddy/study_buddy/settings.py
+++ study_buddy/study_buddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eb(b55=!%c7#63^15t*@c3#bf31%*rgwjegrxeu2i$fqlu)bpf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
